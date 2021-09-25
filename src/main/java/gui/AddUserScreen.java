b67216f65709ba5e6e4f55824c37067c7c372948
Anton2050 <avvsoft2050@gmail.com>
2021-09-26 01:09:14 +0300

    Add GUI Administrate and AddUserScreen

--- src/main/java/gui/AddUserScreen.java
+++ src/main/java/gui/AddUserScreen.java
@@ -3,12 +3,14 @@ package gui;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class AddUserScreen {
     public static void completeUserForm() {
         JFrame frameAddNewUser = new JFrame("Add new User");
-        frameAddNewUser.setBounds(100, 20, 400, 400);
-        frameAddNewUser.setResizable(false);
+        frameAddNewUser.setBounds(100, 20, 500, 400);
+//        frameAddNewUser.setResizable(false);
 //        frameAddNewUser.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
         frameAddNewUser.setLayout(new BorderLayout());
 
@@ -25,20 +27,25 @@ public class AddUserScreen {
         JLabel labelUserBirthDate = new JLabel("Дата рождения:");
         JLabel labelUserEmail = new JLabel("email пользователя:");
         JTextField userName = new JTextField();
-        Dimension textFieldDimension = new Dimension(150, 20);
+        Dimension textFieldDimension = new Dimension(100, 20);
         userName.setPreferredSize(textFieldDimension);
         JTextField userLastName = new JTextField();
         userLastName.setPreferredSize(textFieldDimension);
-        JTextField userBirthDate = new JTextField();
-        userBirthDate.setPreferredSize(textFieldDimension);
+
+        JTextField userBirthDateY = new JTextField();
+        userBirthDateY.setPreferredSize(new Dimension(50, 20));
+        JTextField userBirthDateM = new JTextField();
+        userBirthDateM.setPreferredSize(new Dimension(50, 20));
+        JTextField userBirthDateD = new JTextField();
+        userBirthDateD.setPreferredSize(new Dimension(50, 20));
         JTextField userEmail = new JTextField();
         userEmail.setPreferredSize(textFieldDimension);
 
         GridBagConstraints c = new GridBagConstraints();
         c.fill = GridBagConstraints.BOTH;
+        c.insets = new Insets(0, 0, 5, 5);
         c.gridx = 0;
         c.gridy = 0;
-        c.insets = new Insets(0, 0, 10, 5);
         panel.add(labelUserName, c);
         c.gridx = 1;
         c.gridy = 0;
@@ -54,12 +61,18 @@ public class AddUserScreen {
         panel.add(labelUserBirthDate, c);
         c.gridx = 1;
         c.gridy = 2;
-        panel.add(userBirthDate, c);
-        c.gridx = 0;
-        c.gridy = 3;
-        panel.add(labelUserEmail, c);
+        panel.add(userBirthDateY, c);
         c.gridx = 1;
         c.gridy = 3;
+        panel.add(userBirthDateM, c);
+        c.gridx = 1;
+        c.gridy = 4;
+        panel.add(userBirthDateD, c);
+        c.gridx = 0;
+        c.gridy = 5;
+        panel.add(labelUserEmail, c);
+        c.gridx = 1;
+        c.gridy = 5;
         panel.add(userEmail, c);
 
         JPanel bottomPanel = new JPanel();
@@ -70,6 +83,60 @@ public class AddUserScreen {
         buttonCancel.setPreferredSize(new Dimension(100, 30));
         bottomPanel.add(buttonCreate);
         bottomPanel.add(buttonCancel);
+        buttonCancel.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                frameAddNewUser.dispose();
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+        buttonCreate.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                String name = userName.getText().trim().toUpperCase();
+                String lastName = userLastName.getText().trim().toUpperCase();
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
 
         frameAddNewUser.add(top, BorderLayout.NORTH);
         frameAddNewUser.add(panel, BorderLayout.CENTER);
